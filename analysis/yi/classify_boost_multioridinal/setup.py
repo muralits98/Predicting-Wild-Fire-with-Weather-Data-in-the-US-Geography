b74b8e7f086b25bc7f01686870e4098cb6ab8ddb
Yi Yao <leaf_yao8621@hotmail.com>
2019-11-26 02:28:28 -0500

    fixes

--- analysis/yi/classify_boost_multioridinal/setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, Extension
-import numpy
-
-multiordinal = Extension("multiordinal", sources=["src/multiordinal.c"],
-                         include_dirs=[numpy.get_include()])
-setup(ext_modules=[multiordinal])
